308b6e98fd0140ea7e53b424a3e245241732a825
kexue <xana278@qq.com>
2022-03-22 18:02:33 +0800

    day1

---
+++ day1/EmployeeTest.java
@@ -0,0 +1,21 @@
+package day1;
+
+import java.io.*;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Employee empOne = new Employee("RUNOOB1");
+        Employee empTwo = new Employee("RUNOOB2");
+
+        empOne.empAge(26);
+        empOne.empDesignation("123");
+        empOne.empSalary(1000);
+        empOne.printEmployee();
+
+        empTwo.empAge(21);
+        empTwo.empDesignation("123");
+        empTwo.empSalary(500);
+        empTwo.printEmployee();
+    }
+}
---
+++ day1/helloworld.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
